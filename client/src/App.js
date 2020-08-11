23f00c59adff29eac8fe763dae78222b1630b21e
Yazan <yazankhirfan@gmail.com>
2020-08-10 23:38:49 -0400

    added the post component, action, types, reducer

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
-
+import Posts from './components/posts/Posts';
 import './App.css';
 //Redux
 import { Provider } from 'react-redux';
@@ -70,6 +70,8 @@ const App = () => {
                 path="/add-education"
                 component={AddEducation}
               />
+
+              <PrivateRoute exact path="/posts" component={Posts} />
             </Switch>
           </section>
         </Fragment>
---
+++ client/src/actions/post.js
@@ -0,0 +1,72 @@
+import axios from 'axios';
+import { setAlert } from './alert';
+import { POST_ERROR, GET_POSTS, UPDATE_LIKES, DELETE_POST } from './types';
+
+//GET POSTS
+
+export const getPosts = () => async (dispatch) => {
+  try {
+    const res = await axios.get('/api/posts');
+    dispatch({
+      type: GET_POSTS,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+//ADD A LKE
+
+export const addLike = (id) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/like/${id}`);
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, likes: res.data },
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+//REMOVE A LKE
+
+export const removeLike = (id) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/unlike/${id}`);
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, likes: res.data },
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+//DELETE A POST
+
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    const res = await axios.delete(`/api/posts/${id}`);
+    dispatch({
+      type: DELETE_POST,
+      payload: { id },
+    });
+    dispatch(setAlert('Post Removed', 'Success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
