236dc81b0e9423ba797b5a2e9ca7cd03142351bb
Yazan <yazankhirfan@gmail.com>
2020-08-14 11:44:00 -0400

    prepare for deployment

--- server.js
+++ server.js
@@ -11,14 +11,21 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.send('API running');
-});
-
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+//serve static assests for production
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
