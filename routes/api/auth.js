c5be2e75ab88847e8712216af0d90433b68b48d4
Yazan <yazankhirfan@gmail.com>
2020-08-31 10:35:26 -0400

    added notFound page and created a seperate routes files for all the routes in app.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -56,7 +56,7 @@ router.post(
       jwt.sign(
         payLoad,
         config.get('jwtSecret'),
-        { expiresIn: 360000 },
+        { expiresIn: '5 days ' },
         (err, token) => {
           if (err) throw err;
           res.json({ token });
